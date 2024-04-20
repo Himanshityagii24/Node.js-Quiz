49bdf38292ff69e9f77309cf7706b7dfcebbb932
Himanshi <tyagihimanshi2024@gmail.com>
2024-04-20 21:19:47 +0530

    Final update

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.static('public'));
 
+
+
 const quizQuestions = require('./questions.json');
 
 // Endpoint to serve quiz questions
